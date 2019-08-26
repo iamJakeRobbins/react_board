d2cf50f8dae301890acf4fbeac68ce1431512607
Jake Robbins <jrobbinsd@gmail.com>
2019-08-26 17:02:08 -0600

    test component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/hello.js'
 
 function App() {
   return (
     <div className="App">
+			<Hello name='Jake' />
     </div>
   );
 }
