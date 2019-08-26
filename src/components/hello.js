20fa072a503384bf027143f8030b4603c8462dff
Jake Robbins <jrobbinsd@gmail.com>
2019-08-26 17:08:31 -0600

    modified component to incorporate state

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 class Hello extends React.Component {
+
+  constructor(props) {
+		super(props);
+
+		let firstName = this.props.name.split(' ')[0];
+
+		this.state = {
+			name: firstName,
+		}
+	}
+
 	render() {
-		return <h1> Hello, {this.props.name} </h1>
+		return <h1> Hello, {this.state.name} </h1>
 	}
 }
 
