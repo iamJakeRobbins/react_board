583269837ffa3b839a8e092ef20c192a0544c658
Jake Robbins <jrobbinsd@gmail.com>
2019-08-26 18:11:17 -0600

    changes for redux testing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider as ReduxProvider } from "react-redux";
 import Hello from './components/hello.js'
+import { createStore } from 'redux'
+
+
+// const store = createStore(Hello)
 
 function App() {
   return (
